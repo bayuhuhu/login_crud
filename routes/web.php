987f9c72079738ef7fb9802833a7353758243a73
bayu <bayuheri2709@gmail.com>
2023-02-01 12:04:03 +0700

    commit pertama

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckRole;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home',['title'=>'Home']);
+})->name('home');
+Route::get('register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+Route::get('login', [UserController::class, 'login'])->name('login');
+Route::post('login', [UserController::class, 'login_action'])->name('login.action');
+Route::get('password', [UserController::class, 'password'])->name('password');
+Route::post('password', [UserController::class, 'password_action'])->name('password.action');
+Route::get('logout', [UserController::class, 'logout'])->name('logout');
+
+Route::get('admin', function () { return view('admin'); })->middleware('CheckRole:admin,super');
+Route::get('super', function () { return view('super'); })->middleware(['CheckRole:super,']);
+Route::get('user', function () { return view('user'); })->middleware(['CheckRole:user,admin,super']);
